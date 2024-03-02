bd8b81020116c337816576ab48ba31f5c162e7f8
gnoatto <guifgnoatto@hotmail.com>
2024-03-02 19:27:05 -0300

    Add movement and low poly assets

---
+++ Assets/Scripts/MovementStateManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStateManager : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    [HideInInspector] public Vector3 dir;
+
+    [SerializeField] private float groundYOffset;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpForce = 3f;
+
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private bool grounded;
+
+    private Vector3 spherePos;
+    private float hzInput, vInput;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        GetDirectionMove();
+        Gravity();
+    }
+
+    private void GetDirectionMove()
+    {
+        hzInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        dir = transform.forward * vInput + transform.right * hzInput;
+
+        controller.Move(dir * moveSpeed * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        spherePos = new Vector3(transform.position.x, transform.position.y - groundYOffset, transform.position.z);
+        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return grounded = true;
+
+        return grounded = false;
+    }
+
+    private void Gravity()
+    {
+        if (!IsGrounded())
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            velocity.y = jumpForce;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
+    }
+}
